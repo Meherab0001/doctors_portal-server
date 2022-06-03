68df691c6d188339f7d73430ebb9522a105b2242
Meherab211 <meherab395@gmail.com>
2022-06-03 19:32:55 +0600

    add admin

--- index.js
+++ index.js
@@ -48,6 +48,14 @@ async function run() {
       res.send(users)
     })
 
+    app.get('/admin/:email',async(req,res)=>{
+      const email=req.params.email;
+      const user=await userCollection.findOne({email:email})
+      const isAdmin=user.role ==='admin'
+      res.send(isAdmin)
+    })
+
+
     app.put('/user/admin/:email',verifyJWT,async(req,res)=>{
       const email=req.params.email;
       const requester=req.decoded.email
